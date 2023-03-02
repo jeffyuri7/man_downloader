e742f76bcd53714e1a0ad3745b6f39e13630b2e3
Jefferson Lima <jeffyuri@hotmail.com>
2023-03-02 10:50:54 -0300

    Create main menu for UI

--- lib/interface/user_interface.py
+++ lib/interface/user_interface.py
@@ -9,9 +9,23 @@ class UI:
         """Create a object UI (user interface)."""
         self.title = title
 
+    def line(self, tam=70):
+        """Print a line in UI."""
+        return '-' * tam
+
     def header(self):
         """Create header for UI."""
         print('=' * 70)
         print(str.center(self.title, 70))
         print('=' * 70)
         print("\n")
+
+    def menu(self, options):
+        """Create menu for UI."""
+        num = 1
+        for item in options:
+            print(f'[ {num} ] - {item}')
+            num += 1
+        print(self.line())
+        option = input('Sua opção:  ')
+        return option
