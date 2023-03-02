54c42a5028f4f9b0ce07866c24daa4de1b172b9c
Jefferson Lima <jeffyuri@hotmail.com>
2023-03-02 10:09:20 -0300

    It created user interface class

---
+++ lib/interface/user_interface.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Module to create user interface."""
+
+
+class UI:
+    """Create user interface in terminal, without GUI."""
+
+    def __init__(self, title):
+        """Create a object UI (user interface)."""
+        self.title = title
+
+    def header(self):
+        """Create header for UI."""
+        print('=' * 70)
+        print(str.center(self.title, 70))
+        print('=' * 70)
+        print("\n")
