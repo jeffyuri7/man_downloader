4a0369569242092774c8efc5395717696fb24b8a
Jefferson Lima <jeffyuri@hotmail.com>
2023-03-02 10:07:59 -0300

    Create the first user interface with Header

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
 #!/usr/bin/env python3
+"""Main module - It instancies objects to make the interface and logic."""
 
-print("Teste")
+import os
+from time import sleep
+from lib.interface.user_interface import UI
+
+
+clear = lambda: os.system('clear')
+
+
+def main_principal(opc):
+    """Create the logic of main menu."""
+    clear()
+    if opc == 1:
+        pass
+
+
+while True:
+    try:
+        clear()
+        ui = UI("MANUAL DOWNLOADER")
+        ui.header()
+        opc = int(input("Digite a opção:  "))
+        if opc == 1:
+            continue
+        else:
+            break
+    except:
+        print("Erro, opção inválida! Tente novamente.")
+        sleep(2)
+        continue
