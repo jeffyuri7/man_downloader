aed8a0901b061c0d739c255cb9f0bf3effce0e65
Jefferson Lima <jeffyuri@hotmail.com>
2023-03-03 13:09:23 -0300

    Create database.py file

---
+++ utils/database.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""This module include the logical about database."""
+
+
+class BD:
+    """Create a database object."""
+
+    def __init__(self, banco):
+        """Create a database object."""
+        self.banco = banco
+
+    def insert(self):
+        """Insert data to the database."""
+        pass
+
+    def update(self):
+        """Update the database."""
+        pass
+
+    def select(self):
+        """Select data in database."""
+        pass
+
+    def update_link(self, link):
+        """Update the link if it change."""
+        pass
