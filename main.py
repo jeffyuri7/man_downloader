028eb2d53b8587609bf382b54a3f21aff24346b1
Jefferson Lima <jeffyuri@hotmail.com>
2023-03-03 15:15:21 -0300

    Implement download chapter option

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 
 from manual import Manual
 import list_manual
-import download_manual
 from time import sleep
 from lib.interface.user_interface import UI
 from utils.helper import clear
@@ -13,10 +12,29 @@ def main_principal(opc):
     """Create the logic of main menu."""
     clear()
     if opc == 1:
-        choice = list_manual.main()
-        manual = Manual(choice)
-        print(manual.list_chapters())
-        sleep(3)
+        while True:
+            choice = list_manual.main()
+            manual = Manual(choice)
+            print("\nSelecione a opção desejada:")
+            print("[ 1 ] - Ver a lista de capítulos")
+            print("[ 2 ] - Baixar o Manual Completo")
+            choice2 = ui.inputInt("Sua opção: ")
+            if choice2 == 1:
+                manual.create_index()
+                print(ui.line())
+                print("Digite o número do capítulo desejado para baixar, ou digite 0 para baixar o manual completo.")
+                choice3 = ui.inputInt("Sua Opção: ")
+                if choice3 == 0:
+                    manual.download_manual()
+                    break
+                manual.download_chapter(choice3)
+            elif choice2 == 3:
+                manual.download_manual()
+                break
+            else:
+                print("Opção inválida! Tente novamente.")
+                continue
+            sleep(3)
 
 
 while True:
