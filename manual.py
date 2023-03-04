095dedcff7d5cede712f67da8562dabfde40bc09
Jefferson Lima <jeffyuri@hotmail.com>
2023-03-04 12:55:23 -0300

    Integration with download_manual and helper.py

--- manual.py
+++ manual.py
@@ -47,7 +47,6 @@ class Manual:
             lista.append(key)
         self.index = lista
 
-
     def download_manual(self):
         """Download the entire manual."""
         pass
@@ -61,23 +60,22 @@ class Manual:
         # Request URL and get response object
         link = self.index[chapter-1]
         url = self.dictionary[link]
-        print(url)
-        sleep(4)
         response = requests.get(url, stream=True)
-
+        folder = self.create_folder()
         # isolate DOC filename from URL
         doc_file_name = os.path.basename(url)
         if response.status_code == 200:
-            # Save in current working directory
-            filepath = os.path.join(os.getcwd(), doc_file_name)
+            # Save in new directory created by create_folder function
+            filepath = os.path.join(folder, doc_file_name)
             with open(filepath, 'wb') as doc_object:
                 doc_object.write(response.content)
                 print(f'{doc_file_name} foi baixado com sucesso!')
                 return True
         else:
-            print(f'Uh oh! Não conseguimos baixar {doc_file_name}')
+            print(f'Ops! Não conseguimos baixar {doc_file_name}')
             print(f"Resposta do servidor: {response.status_code}")
             return False
+
     def create_folder(self):
         """Create a folder to save the manual."""
         title = first_word(self.titulo)  # Create a folder with first word
