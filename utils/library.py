b30286faaf78203543e6d2a1eb42877c168a1937
Jefferson Lima <jeffyuri@hotmail.com>
2023-03-05 16:46:47 -0300

    Create class Library

---
+++ utils/library.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Class module to create a library of manuals."""
+
+
+class Library:
+    """Create a library of manuals."""
+
+    def __init__(self, db):
+        """Construct the library object."""
+        self.all_manuals = []
+
+    def list_manuals(self):
+        """List all manuals of library."""
+        pass
+
+    def download_all_manuals(self):
+        """Download all manuals of library."""
+        pass
+
+    def add_manual(self):
+        """Add one manual to library."""
+        pass
+
+    def remove_manual(self):
+        """Remove one manual of library."""
+        pass
