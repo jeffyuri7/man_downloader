2f95915faa740a8ce838f85d38c0c2b15c5e4c7c
Jefferson Lima <jeffyuri@hotmail.com>
2023-03-07 09:52:43 -0300

    Implement update_library function
    This function now update the local database to mirror the server. If
    there are differences this function treats it adding or removing
    such data.

--- library.py
+++ library.py
@@ -30,18 +30,35 @@ class Library:
         """Add one manual to library."""
         print("Sincronizando a lista de manuais com o servidor...")
         try:
+            # First, check if self.all_manuals alredy exists. If it doesn't
+            # exist we call list_manuals function that set this var.
             if not self.all_manuals:
                 self.list_manuals(db)
-            list_currently = []
-            for item in self.all_manuals:
-                list_currently.append((item[1], item[2]))
+
+            # Create a iter with data to compare with download data.
+            list_currently = [(item[1], item[2]) for item in self.all_manuals]
+
+            # Retrieve link of local database.
             self.link = db.consult_link()
+
+            # Start request to server.
             res = requests.get(self.link)
             res.raise_for_status()
             manuals_bs4 = bs4.BeautifulSoup(res.text, features="lxml")
             manuals = manuals_bs4.select('.titulo-marcadores-1 > a[href]')
+
+            # Create a iter with download data.
             index_library = [((item.getText()).strip(), (item.get('href')).strip()) for item in manuals]
-            db.update_library(index_library)
+
+            # Compare the two iter. In first comparison the difference
+            # will be add to database. In second comparison the difference
+            # will be delete of database. This will become the local database
+            # and server equals.
+            data_add = list(set(index_library).difference(list_currently))
+            data_rem = [(item[0],) for item in (list(set(list_currently).difference(index_library)))]
+
+            # Call function that realize the update in local database.
+            db.update_library(data_add, data_rem)
         except Exception as exc:
             print(exc)
             sleep(3)
